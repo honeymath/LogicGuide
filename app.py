070e6e468399167593b8b0125ae1a0dccf4e18ff
honeymath <qqzipcar@gmail.com>
2024-06-25 08:31:41 +0900

    m

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    path = os.getenv('PATH')
-    return f'PATH: {path}'
+    google_client_id = os.getenv('GOOGLE_CLIENT_ID')
+    google_client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+    return f'GOOGLE_CLIENT_ID: {google_client_id}, GOOGLE_CLIENT_SECRET: {google_client_secret}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
