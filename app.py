65936f9fccddebf253070454b3bc010235533c40
honeymath <qqzipcar@gmail.com>
2024-06-25 08:44:03 +0900

    mu

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
+def load_credentials(file_path):
+    credentials = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            name, value = line.strip().split('=')
+            credentials[name] = value
+    return credentials
+
+credentials = load_credentials('credentials.txt')
+
 @app.route('/')
 def index():
-    google_client_id = os.getenv('GOOGLE_CLIENT_ID')
-    google_client_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+    google_client_id = credentials.get('GOOGLE_CLIENT_ID')
+    google_client_secret = credentials.get('GOOGLE_CLIENT_SECRET')
     return f'GOOGLE_CLIENT_ID: {google_client_id}, GOOGLE_CLIENT_SECRET: {google_client_secret}'
 
 if __name__ == '__main__':
