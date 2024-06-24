56893124a304b12d338c1bf4a0f9fee3bec34c5f
honeymath <qqzipcar@gmail.com>
2024-06-24 11:30:27 +0900

    ha

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = client.mydatabase
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Furword'
+    return 'This is github speaking. Please answer'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
