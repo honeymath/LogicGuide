ee3c46bf6bdd5f262337ca819ebc709ff4b8a5a4
honeymath <qqzipcar@gmail.com>
2024-06-24 10:40:58 +0900

    furst commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.mydatabase
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
